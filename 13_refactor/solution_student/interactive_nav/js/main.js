b6fad7ad72b77dedc47a3b39403d28d74db36b50
Hannah <hannah.demilta@gmail.com>
2016-01-31 15:56:11 -0600

    classwork

--- 13_refactor/solution_student/interactive_nav/js/main.js
+++ 13_refactor/solution_student/interactive_nav/js/main.js
@@ -2,4 +2,8 @@
 //Click on menu item and show corresponding second menu bar and remove other secondary menu options
 
 
-$()
+$('#main-nav a').on('click,' function () {
+	$('nav.active a').removeClass('active'); 
+	$(this).addClass('active');
+
+}); 
